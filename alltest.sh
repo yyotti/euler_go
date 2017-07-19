77f5793230431ba083de2ab2769fea8aae5e60a7
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2017-07-19 11:17:53 +0900

    一括テストのためのシェルスクリプトを追加

---
+++ alltest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE:-$0}")"
+# go test "$(go list ./...)"
+echo GOPATH "$GOPATH"
+echo GOROOT "$GOROOT"
+find ./src -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 go test
