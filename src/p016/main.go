95fab737df03a5ac7c39d89578d3e119bf759144
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2017-07-23 14:40:46 +0900

    P016を実装

---
+++ src/p016/main.go
@@ -0,0 +1,98 @@
+package main
+
+import "fmt"
+import "github.com/yyotti/euler_go/src/common"
+import "math/big"
+
+// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//
+// What is the sum of the digits of the number 2^1000?
+
+const e = 1000
+
+func main() {
+	fmt.Printf("P016A: %d\n", p016A(e))
+	fmt.Printf("P016B: %d\n", p016B(e))
+	fmt.Printf("P016C: %d\n", p016C(e))
+}
+
+// 普通にやったらどう考えてもオーバーフローするのでライブラリ使う
+func p016A(e uint) uint {
+	b := big.NewInt(2)
+	b.Exp(b, big.NewInt(int64(e)), nil)
+
+	ns, _ := common.SplitNums(b.Text(10))
+	sum := uint(0)
+	for _, d := range ns {
+		sum += d
+	}
+
+	return sum
+}
+
+// ライブラリを使わずにやる(1)
+func p016B(e uint) uint {
+	mul := "1"
+	for i := uint(1); i <= e; i++ {
+		mul = common.Mul(mul, "2")
+	}
+
+	sum := uint(0)
+	ds, _ := common.SplitNums(mul)
+	for _, d := range ds {
+		sum += d
+	}
+
+	return sum
+}
+
+// ライブラリを使わずにやる(2)
+//
+// 文字列の掛け算を1より減らしてみる
+func p016C(e uint) uint {
+	ms := []uint{}
+	k := e
+	for {
+		i := uint(0)
+		for ; (1 << i) <= k; i++ {
+		}
+		if i == 0 {
+			break
+		}
+		ms = append(ms, i)
+		k -= i
+	}
+
+	ns := make([]string, 0, len(ms))
+	for _, m := range ms {
+		ns = append(ns, pow2(m))
+	}
+
+	mul := "1"
+	for _, n := range ns {
+		mul = common.Mul(mul, n)
+	}
+
+	ds, _ := common.SplitNums(mul)
+	sum := uint(0)
+	for _, d := range ds {
+		sum += d
+	}
+
+	return sum
+}
+
+func pow2(e uint) string {
+	if e == 0 {
+		return "1"
+	}
+
+	if e%2 == 1 {
+		p := pow2((e - 1) / 2)
+		p = common.Mul(p, p)
+		return common.Mul("2", p)
+	}
+
+	p := pow2(e / 2)
+	return common.Mul(p, p)
+}
---
+++ src/p016/main_test.go
@@ -0,0 +1,98 @@
+package main
+
+import "testing"
+
+var pow2Tests = []struct {
+	input    uint
+	expected string
+}{
+	{input: 0, expected: "1"},
+	{input: 1, expected: "2"},
+	{input: 2, expected: "4"},
+	{input: 3, expected: "8"},
+	{input: 4, expected: "16"},
+	{input: 5, expected: "32"},
+	{input: 6, expected: "64"},
+	{input: 7, expected: "128"},
+	{input: 8, expected: "256"},
+	{input: 9, expected: "512"},
+	{input: 10, expected: "1024"},
+}
+
+func TestPow2(t *testing.T) {
+	for _, tt := range pow2Tests {
+		actual := pow2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("%d: Expected %s but got %s", tt.input, tt.expected, actual)
+		}
+	}
+}
+
+var p016Tests = []struct {
+	input    uint
+	expected uint
+}{
+	{input: 0, expected: 1},
+	{input: 1, expected: 2},
+	{input: 2, expected: 4},
+	{input: 3, expected: 8},
+	{input: 4, expected: 7},
+	{input: 5, expected: 5},
+	{input: 6, expected: 10},
+	{input: 7, expected: 11},
+	{input: 15, expected: 26},
+}
+
+func TestP016A(t *testing.T) {
+	for _, tt := range p016Tests {
+		actual := p016A(tt.input)
+		if actual != tt.expected {
+			t.Errorf("%d: Expected %d but got %d", tt.input, tt.expected, actual)
+		}
+	}
+}
+
+func TestP016B(t *testing.T) {
+	for _, tt := range p016Tests {
+		actual := p016B(tt.input)
+		if actual != tt.expected {
+			t.Errorf("%d: Expected %d but got %d", tt.input, tt.expected, actual)
+		}
+	}
+}
+
+func TestP016C(t *testing.T) {
+	for _, tt := range p016Tests {
+		actual := p016C(tt.input)
+		if actual != tt.expected {
+			t.Errorf("%d: Expected %d but got %d", tt.input, tt.expected, actual)
+		}
+	}
+}
+
+func BenchmarkP016A(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		p016A(100)
+	}
+}
+
+func BenchmarkP016B(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		p016B(100)
+	}
+}
+
+func BenchmarkP016C(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		p016C(100)
+	}
+}
+
+func ExampleP016() {
+	main()
+
+	// Output:
+	// P016A: 1366
+	// P016B: 1366
+	// P016C: 1366
+}
